6cb11a9ac438c8a66feaffcbd9ecf9ebc1e52f74
ali aborady <aboradyali48@gmail.com>
2026-03-15 00:43:20 +0200

    Add project files.

---
+++ ForthApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+using System.Reflection.Emit;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ForthApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
